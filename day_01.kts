783c0548bf47b86c09540e558389d76f2b3f642b
mangefox <fox@fojji.se>
2017-12-02 01:13:35 +0100

    Adds generic step solution to day_01

--- day_01.kts
+++ day_01.kts
@@ -1,18 +1,10 @@
 import java.io.File
 
-// problem 1
-fun sumSequence1(digits: List<Int>) =
-        (digits + digits.first())
-                .zipWithNext { x, y -> if (x == y) x else 0 }
-                .sum()
+fun captcha(input: List<Int>, step: Int) =
+        input.indices.filter { input[it] == input[(it+step) % input.size] }
+                     .map { input[it] }
+                     .sum()
 
-// problem 2
-fun sumSequence2(digits: List<Int>) =
-        (digits + digits.take(digits.size / 2))
-                .windowed(1 + digits.size / 2)
-                .map { if (it.first() == it.last()) it.first() else 0 }
-                .sum()
-
-val digits = File("day_01_input").readText().map { it.toInt()-48 }
-println("solution 1: ${sumSequence1(digits)}") // 1393
-println("solution 2: ${sumSequence2(digits)}") // 1292
+val digits = File("day_01_input").readText().map { it-'0' }
+println("solution 1: ${captcha(digits, 1)}") // 1393
+println("solution 2: ${captcha(digits, digits.size/2)}") // 1292
