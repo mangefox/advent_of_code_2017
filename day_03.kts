bb1984f1d5a585d7ba1f789203aff613dd724bc4
mangefox <fox@fojji.se>
2017-12-03 23:21:36 +0100

    Added solution for day 3

---
+++ day_03.kts
@@ -0,0 +1,39 @@
+import java.io.File
+import kotlin.math.*
+
+fun spiral1(input: Int): Int {
+    val sqrt = ceil(sqrt(input.toDouble())).toInt() // find root of closest square higher than input
+    val corner = sqrt*sqrt // value of the "bottom right" corner
+    val corners = (0..3).map { corner-(sqrt-1)*it } // get values of all four corners
+    val distToCorner = corners.map { abs(it-input) }.min() // find distance to closest corner
+    return ((sqrt-1)-distToCorner!!) // corners are sqrt-1 distance from center
+}
+
+fun spiral2(input: Int): Int {
+    val mid = 20
+    val matrix = Array(mid*2) { IntArray(mid*2) }
+    matrix[mid][mid] = 1
+
+    fun adjacentSum(row: Int, col: Int) =
+            (-1..1).sumBy { i -> (-1..1).sumBy { j -> matrix[row+i][col+j] } }
+
+    var (x, y, dir, dist) = listOf(0, 0, 1, 1)
+    while (true) {
+        while (y * dir * 2 < dist) {
+            val value = adjacentSum(mid-x, mid+y)
+            if (value > input) return value else matrix[mid-x][mid+y] = value
+            y += dir
+        }
+        while (x * dir * 2 < dist) {
+            val value = adjacentSum(mid-x, mid+y)
+            if (value > input) return value else matrix[mid-x][mid+y] = value
+            x += dir
+        }
+        dir *= -1
+        dist += 1
+    }
+}
+
+val input = 325489
+println("solution 1: " + spiral1(input))
+println("solution 2: " + spiral2(input))
