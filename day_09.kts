4e744a92da004e149545065bacddb217fbb2b9cf
mangefox <fox@fojji.se>
2017-12-09 23:37:11 +0100

    Added solution for day 9

---
+++ day_09.kts
@@ -0,0 +1,54 @@
+import java.io.File
+import java.util.*
+
+data class Result(val score: Int, val garbage: Int)
+
+fun process(input: String): Result {
+    var score = 1
+    var totalScore = 0
+    var garbage = false
+    var garbageCounter = 0
+    val stack = LinkedList<Int>()
+
+    var i = 0
+    while (i in input.indices) {
+        when (input[i]) {
+            '!' -> i++
+            '<' -> if (!garbage) garbage = true
+                   else garbageCounter += 1
+            '>' -> garbage = false
+            '{' -> if (!garbage) stack.push(score++)
+                   else garbageCounter += 1
+            '}' -> if (!garbage) {
+                       totalScore += stack.pop()
+                       score--
+                   }  else garbageCounter += 1
+            else -> if (garbage) garbageCounter += 1
+        }
+        i++
+    }
+    return Result(totalScore, garbageCounter)
+}
+
+val input = File("day_09_input").readText()
+
+println("solution 1: " + process(input).score)
+println("solution 2: " + process(input).garbage)
+
+require(process("{}").score == 1)
+require(process("{{{}}}").score == 6)
+require(process("{{},{}}").score == 5)
+require(process("{{{},{},{{}}}}").score == 16)
+require(process("{<a>,<a>,<a>,<a>}").score == 1)
+require(process("{{<ab>},{<ab>},{<ab>},{<ab>}}").score == 9)
+require(process("{{<!!>},{<!!>},{<!!>},{<!!>}}").score == 9)
+require(process("{{<a!>},{<a!>},{<a!>},{<ab>}}").score == 3)
+
+require(process("<>").garbage == 0)
+require(process("<random characters>").garbage == 17)
+require(process("<<<<>").garbage == 3)
+require(process("<{!>}>").garbage == 2)
+require(process("<!!").garbage == 0)
+require(process("<!!!>>").garbage == 0)
+require(process("<{o\"i!a,<{i<a>").garbage == 10)
+
